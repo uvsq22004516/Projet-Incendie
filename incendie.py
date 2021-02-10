b711891466749eb88a755e0bd093b0cdefec54cf
uvsq22004516 <gabriel.champenois@ens.uvsq.fr>
2021-02-11 00:38:16 +0100

    format du code

--- incendie.py
+++ incendie.py
@@ -1,4 +1,4 @@
-#########################################
+################## informations liées au groupe #######################
 
 # groupe DLMP 
 # Gabriel CHAMPENOIS
@@ -12,5 +12,30 @@
 
 #########################################
 
-print("a")
-test réussi
+
+
+################## import des librairies #######################
+    
+
+
+################## définition des constantes #######################
+WIDTH = 600
+HEIGHT = 400
+
+
+################## définition des variables globales #######################
+"""Eau 	Bleu 	 + ∞
+   Forêt 	Vert 	Dépend des voisins
+   Feu 	Rouge 	Constante DUREE_FEU
+   Prairie 	Jaune 	Dépend des voisins
+   Cendres tièdes 	Gris 	Constante DUREE_CENDRE
+   Cendres éteintes 	Noir 	 + ∞"""
+
+
+################## définition des fonctions (avec docstring) #######################
+
+
+
+################## programme principal contenant la définition des widgets 
+# et des événements qui leur sont liés et l’appel à la boule de gestion des événements #######################
+    
