c8f5f9c5f7bff18d4224209ba074e800d31bb78a
uvsq22004516 <gabriel.champenois@ens.uvsq.fr>
2021-02-15 12:29:47 +0100

    edit README + fioritures code

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# Projet-Incendie super, j'ai pas lu la consigne!
-# Nice
-# Test
-
-# Modif Via Git
+# Simulation d'incendie - Détails d'utilisation du programme:
+#
+#       - Règles d'évolution:
+#               • Une parcelle d’eau reste une parcelle d’eau durant toute la simulation;
+#               • Une parcelle qui devient en feu reste en feu durant la durée DUREE_FEU avant de devenir des cendres tièdes pendant la durée DUREE_CENDRE et enfin devenir des cendres éteintes durant le reste de la simulation; 
+#               • Une parcelle de prairie prend feu dès qu’une des 4 cases voisines (gauche, droite, haut, bas) est en feu;
+#               • Une parcelle de forêt prend feu avec la probabilité 0.1×nf où nf est le nombre de ses voisins en feu
+#
+#
+#       - Vitesse de la simulation:
+#               • Accélérer la vitesse de la simultation: touche "a"
+#               • Réduire la vitesse de la simulation: touche "r"
+#
+#
+#       - Différents boutons: 
+#               •"NOM_BOUTON" --> bouton qui génère un terrain au hasard avec des parcelles d’eau, de forêt et de prairie
+#               •"NOM_BOUTON" --> bouton pour sauvegarder l’état du terrain dans un fichier
+#               •"NOM_BOUTON" --> bouton pour charger un terrain depuis un fichier
+#               •"NOM_BOUTON" --> bouton qui permet d'effectuer une étape de simulation (raccourci: touche "s")
+#               •"NOM_BOUTON" --> bouton qui permet de démarrer une simulation
+#               •"NOM_BOUTON" --> bouton pour arrêter la simulation
--- incendie.py
+++ incendie.py
@@ -1,6 +1,6 @@
 ################## informations liées au groupe #######################
 
-# groupe DLMP 
+# groupe DLMP 5
 # Gabriel CHAMPENOIS
 # Alexis BODEN
 # Lucas LEMARECHAL
@@ -15,27 +15,40 @@
 
 
 ################## import des librairies #######################
-    
+import tkinter as tk
+import random as rd
+
+root = tk.Tk()
+root.title("Simulation d'incendie (IN200 - projet n°1)")
 
 
-################## définition des constantes #######################
-WIDTH = 600
-HEIGHT = 400
-
+################## définition des constantes (en MAJ) #######################
+WIDTH = 900
+HEIGHT = 600
+DUREE_FEU = 0
+DUREE_CENDRE = 0
 
 ################## définition des variables globales #######################
-"""Eau 	Bleu 	 + ∞
-   Forêt 	Vert 	Dépend des voisins
-   Feu 	Rouge 	Constante DUREE_FEU
-   Prairie 	Jaune 	Dépend des voisins
-   Cendres tièdes 	Gris 	Constante DUREE_CENDRE
-   Cendres éteintes 	Noir 	 + ∞"""
+"""Eau 	            Bleu 	   + ∞
+   Forêt 	         Vert 	   Dépend des voisins
+   Feu 	            Rouge 	Constante DUREE_FEU
+   Prairie 	         Jaune 	Dépend des voisins
+   Cendres tièdes 	Gris 	   Constante DUREE_CENDRE
+   Cendres éteintes 	Noir 	   + ∞"""
 
 
 ################## définition des fonctions (avec docstring) #######################
 
 
 
-################## programme principal contenant la définition des widgets 
-# et des événements qui leur sont liés et l’appel à la boule de gestion des événements #######################
-    
+################## définition des widgets ##################
+
+terrain = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
+
+
+
+
+################## événements liés aux widgets et appel à la boule de gestion des événements #######################
+
+terrain.grid()
+root.mainloop()
