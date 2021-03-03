0a1832c5ecc161bc16a0c87eff1a06cefe831967
uvsq22004516 <gabriel.champenois@ens.uvsq.fr>
2021-03-04 00:28:08 +0100

    ajout et placement des boutons

--- incendie.py
+++ incendie.py
@@ -23,8 +23,8 @@ root.title("Simulation d'incendie (IN200 - projet n°1)")
 
 
 ################## définition des constantes (en MAJ) #######################
-WIDTH = 900
-HEIGHT = 600
+WIDTH = 1000
+HEIGHT = 650
 DUREE_FEU = 0
 DUREE_CENDRE = 0
 
@@ -39,16 +39,46 @@ DUREE_CENDRE = 0
 
 ################## définition des fonctions (avec docstring) #######################
 
+def generer_terrain():
+   pass
 
+def sauver_terrain():
+   pass
+
+def charg_terrain():
+   pass
+
+def effect_etape():
+   pass
+
+def debut_simulation():
+   pass
+
+def arret_simulation():
+   pass
 
 ################## définition des widgets ##################
 
 terrain = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="white")
 
 
-
+generer_Terrain = tk.Button(root, command=generer_terrain, text="Générer un terrain", font=("Times", "10", "bold"), relief="ridge", bd=5)
+sauver_Terrain = tk.Button(root, command=sauver_terrain, text="Sauvegarder l'état du terrain", font=("Times", "10", "bold"), relief="ridge", bd=5)
+charger_Terrain = tk.Button(root, command=charg_terrain, text="Charger un terrain", font=("Times", "10", "bold"), relief="ridge", bd=5)
+effectuer_Etape = tk.Button(root, command=effect_etape, text="Effectuer une étape", font=("Times", "10", "bold"), relief="ridge", bd=5)
+demarrer_Simulation = tk.Button(root, command=debut_simulation, text="Démarrer la simulation", font=("Times", "10", "bold"), relief="ridge", bd=5)
+arreter_Simulation = tk.Button(root, command=arret_simulation, text="Arrêter la simulation",font=("Times", "10", "bold"), relief="ridge", bd=5)
 
 ################## événements liés aux widgets et appel à la boule de gestion des événements #######################
 
-terrain.grid()
+terrain.grid(column=1, columnspan=2, row=0, rowspan=3)
+generer_Terrain.grid(column=0, row=0)
+sauver_Terrain.grid(column=0, row=1)
+charger_Terrain.grid(column=0, row=2)
+effectuer_Etape.grid(column=3, row=1)
+demarrer_Simulation.grid(column=1, row=3)
+arreter_Simulation.grid(column=2, row=3)
+
+
+
 root.mainloop()
